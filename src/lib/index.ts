4dc74a2ce80d0088ec366358ed6033e3aa5fe864
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-05 08:06:03 +0700

    export library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
-// Reexport your entry components here
+import Code from './components/Code.svelte'
+import Notes from './components/Notes.svelte'
+import Presentation from './components/Presentation.svelte'
+import Slide from './components/Slide.svelte'
+import Youtube from './components/embed/Youtube.svelte'
+
+export { Code, Notes, Presentation, Slide, Youtube }
+
