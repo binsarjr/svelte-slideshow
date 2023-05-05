3182bd70914db34f8409d47b3059727199aa3d06
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-05 07:31:48 +0700

    component init

---
+++ src/lib/components/Presentation.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import 'reveal.js/dist/reveal.css';
+	import 'reveal.js/dist/theme/black.css';
+	import { onMount } from 'svelte';
+	import type Reveal from 'reveal.js';
+
+	export let config: Reveal.Options | undefined = undefined;
+	export let height = '100%';
+	export let width = '100%';
+
+	let el: HTMLDivElement;
+	let api: Reveal.Api;
+	onMount(async () => {
+		const RevealJs = (await import('reveal.js')).default;
+		// @ts-ignore
+		api = new RevealJs(el, config);
+		api.initialize();
+	});
+</script>
+
+<div bind:this={el} style:height style:width class="reveal">
+	<div class="slides">
+		<slot />
+	</div>
+</div>
---
+++ src/lib/components/embed/Youtube.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let url: string;
+	export let width = 560;
+	export let height = 315;
+	const regex = /https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9]*)/;
+	let id: string;
+	const found = url.match(regex);
+	if (found) {
+		[, id] = found;
+	}
+</script>
+
+<iframe
+	{width}
+	{height}
+	src="https://www.youtube.com/embed/{id}"
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+/>
