f9629f93240781febda2448dc22492f43fb9600c
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-12 23:43:18 +0700

    add: markdown slide

---
+++ src/lib/components/SlideMarkdown.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let url = '';
+	export let seperator = '';
+	export let seperatorVertical = '';
+	export let seperatorNotes = '';
+	export let charset = '';
+</script>
+
+<section
+	data-markdown={url}
+	data-separator={seperator}
+	data-separator-vertical={seperatorVertical}
+	data-separator-notes={seperatorNotes}
+	data-charset={charset}
+>
+	<slot />
+</section>
